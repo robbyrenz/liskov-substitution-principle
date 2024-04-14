a1b733a4b78c5073e2d7ca556794573a8f02530e
Robby <nxgrenz@gmail.com>
2024-04-14 16:04:00 -0500

    refine the animations in the first code example

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -3,11 +3,14 @@ import { createRef, beginSlide, slideTransition, Direction, DEFAULT, all, waitFo
 import fry from '../../images/fry.png'
 
 export default makeScene2D(function*(view) {
+  // set the default color of the background
   view.fill('#1d3557');
+
+  // set the transtion to swipe from the right into this new slide
   yield* slideTransition(Direction.Right, 2)
 
+  // create some code
   const code = createRef<Code>()
-
   view.add(
     <Code
       ref={code}
@@ -17,8 +20,8 @@ export default makeScene2D(function*(view) {
     />
   )
 
-  yield* beginSlide('The one that shows the code!');
-
+  // show the first code snippet
+  yield* beginSlide('Show the Engine base class');
   yield* code().code.append(`\
 class Engine {
   protected int cost() {
@@ -26,8 +29,8 @@ class Engine {
   }
 }`, 1);
 
-  yield* beginSlide('first')
-
+  // show the second code snippet
+  yield* beginSlide('Show the PistonEngine subclass')
   yield* code().code.append(`\n
 class PistonEngine extends Engine {
   @Override
@@ -36,17 +39,16 @@ class PistonEngine extends Engine {
   }
 }`, 0.6)
 
-  yield* beginSlide('selecting code')
-  yield* all(
-    code().selection(lines(1, 3), 0.6),
-    code().selection(lines(8, 10), 0.6)
-  )
-
-  yield* beginSlide('selecting the access modifiers')
+  // highlight the protected and public access modifiers
+  yield* beginSlide('Highlight the access modifiers')
   yield* code().selection(code().findAllRanges(/protected|public/gi), 0.6)
 
-  yield* beginSlide('clearing the selections and showing the third snippet')
+  // clear the highlights
+  yield* beginSlide('Clear the highlights')
   yield* code().selection(DEFAULT, 0.6)
+
+  // show the third code snippet
+  yield* beginSlide('Show the TurboEngine subclass')
   yield* code().code.append(`\n
 class TurboEngine extends Engine {
   @Override
@@ -55,25 +57,40 @@ class TurboEngine extends Engine {
   }
 }`, 0.6)
 
-  yield* beginSlide('selecting another set of access modifiers')
+  // highlight the protected and private access modifiers
+  yield* beginSlide('Highlight the access modifiers')
   yield* code().selection(code().findAllRanges(/protected|private/gi), 0.6)
-  yield* beginSlide('clear the selections')
+
+  // clear the highlights again
+  yield* beginSlide('Clear the highlights')
   yield* code().selection(DEFAULT, 0.6)
 
-
-  yield* beginSlide('add in the compiler error as a comment!')
-  yield* code().code.insert([15, 24], '// a compiler error is thrown due to a violation of the LSP!\n', 0.6)
-
-  // image stuff
+  // insert the "suspicious Fry" meme out of view :)
   yield* beginSlide('Insert the Fry meme')
   const suspiciousFryMeme = createRef<Img>();
-  view.add(<Img ref={suspiciousFryMeme} src={fry} scale={2} />);
-  yield* waitFor(1.5)
-  yield* suspiciousFryMeme().scale(2.5, 1.5).to(2, 1.5)
+  view.add(
+    <Img
+      ref={suspiciousFryMeme}
+      src={fry}
+      scale={2}
+      x={-2000}
+    />
+  );
+  yield* suspiciousFryMeme().position.x(0, 1) // and then move Fry to the center of the screen
+
+  // move Fry to the side so that we won't block the code snippets
+  yield* beginSlide('Move Fry to the side')
+  yield* suspiciousFryMeme().scale(2.5, 1.5) // fancy scaling work just to be dramatic
   yield* all(
     suspiciousFryMeme().scale(1, 2),
     suspiciousFryMeme().position.x(500, 1.5),
     suspiciousFryMeme().position.y(-200, 1.5)
   )
-  // suspiciousFryMeme().absoluteRotation(90, 1.5).to(0, 1.5)
+
+  // appending the compiler error as a comment to the code
+  yield* beginSlide('add in the compiler error as a comment!')
+  yield* code().code.insert([15, 24], '// a compiler error is thrown due to a violation of the LSP!\n', 0.6)
+
+  // wait until I press the space bar in order to head to the next slide and transition
+  yield* beginSlide('Head to the next slide and transition')
 })
