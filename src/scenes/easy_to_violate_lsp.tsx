fb8fed12ed984d3abd608d29fe62f81feb722ff9
Robby <nxgrenz@gmail.com>
2024-04-25 08:06:59 -0500

    insert the Fry meme again in the last slide!

--- src/scenes/easy_to_violate_lsp.tsx
+++ src/scenes/easy_to_violate_lsp.tsx
@@ -2,6 +2,7 @@ import { Code, Img, makeScene2D, word } from '@motion-canvas/2d';
 import { createRef, beginSlide, slideTransition, Direction, all } from '@motion-canvas/core';
 import { BACKGROUND_COLOR } from '../palettes';
 import haroldMeme from '../../images/harold_two.png'
+import fry from '../../images/fry.png'
 
 export default makeScene2D(function*(view) {
   // set the default color of the background
@@ -66,6 +67,28 @@ public class Foo {
   yield* all(
     code().code.replace(word(10, 17, 12), '', 0.6),
     code().code.insert([11, 0], "  private final Foo foo = new Foo();\n\n  public int getAge() {\n    return foo.getAge();\n  }\n\n  public String getCountry() {\n    return foo.getCountry();\n  }\n", 0.6),
+  )
+
+  // insert the "suspicious Fry" meme out of view, again, coming back into full circle :)
+  yield* beginSlide('Insert the Fry meme, again!')
+  const suspiciousFryMeme = createRef<Img>();
+  view.add(
+    <Img
+      ref={suspiciousFryMeme}
+      src={fry}
+      scale={2}
+      x={-2000}
+    />
+  );
+  yield* suspiciousFryMeme().position.x(0, 1) // move Fry to the center of the screen
+
+  // move Fry to the side so that we won't block the code snippets
+  yield* beginSlide('Move Fry to the side')
+  yield* suspiciousFryMeme().scale(2.5, 1.5) // fancy scaling work just to be dramatic
+  yield* all(
+    suspiciousFryMeme().scale(1, 2),
+    suspiciousFryMeme().position.x(500, 1.5),
+    suspiciousFryMeme().position.y(-200, 1.5)
   )
 
   // wait until I press the space bar in order to head to the next slide and transition
