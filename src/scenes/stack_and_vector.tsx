e3fe71a43005cb639ed89cfdc43def4776f7b434
Robby <nxgrenz@gmail.com>
2024-04-25 08:02:41 -0500

    animate balls flying away
    I also inserted a code snippet that shows the violation of the OCP.

--- src/scenes/stack_and_vector.tsx
+++ src/scenes/stack_and_vector.tsx
@@ -1,5 +1,5 @@
-import { Circle, makeScene2D } from '@motion-canvas/2d';
-import { createRef, beginSlide, slideTransition, Direction, chain, waitFor } from '@motion-canvas/core';
+import { Circle, Code, makeScene2D, word } from '@motion-canvas/2d';
+import { createRef, beginSlide, slideTransition, Direction, chain, waitFor, all } from '@motion-canvas/core';
 import { BACKGROUND_COLOR } from '../palettes';
 
 export default makeScene2D(function*(view) {
@@ -79,6 +79,43 @@ export default makeScene2D(function*(view) {
     circleFive().position.y(-1000, 2),
     circleFive().position.y(-95, 1.5),
   );
+
+  // remove the balls
+  yield* beginSlide('Remove the balls');
+  yield* all(
+    circleOne().position.x(-1500, 1.5),
+    cirlceTwo().position.x(1500, 1),
+    circleThree().position.x(-1500, 2),
+    circleFour().position.x(1500, 1.5),
+    circleFive().position.x(-1500, 1),
+  )
+
+  // create a method that takes in a Vector argument
+  yield* beginSlide('Create a method that takes in a Vector argument');
+  const vector = createRef<Code>()
+  view.add(
+    <Code
+      ref={vector}
+      fontSize={42}
+      offsetX={-1}
+      x={-900}
+    />
+  )
+
+  yield* vector().code.append(`\
+public void fn(Vector vector) {
+  // do something
+}`, 1)
+
+
+  // insert the 'instanceOf' keyword and format the code appropriately
+  yield* beginSlide('Insert the "instanceOf" keyword')
+  yield* vector().code.insert([1, 0], "\
+  if (vector instanceOf Stack) {\n\
+    throw new IllegalArgumentException();\n\
+  } else {\n", 0.6);
+  yield* vector().code.insert([5, 0], "  }\n", 0.6)
+  yield* vector().code.replace(word(4, 2), '  // do something\n', 0.6)
 
   // wait until I press the space bar in order to head to the next slide and transition
   yield* beginSlide('Head to the next slide and transition')
